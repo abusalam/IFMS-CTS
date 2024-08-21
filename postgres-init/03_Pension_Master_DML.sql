a89617fab97ea37d6d894de14db1ddc6f7fcc7d2
Abu Salam [Lenovo-Slim3i] <abu.alam@nic.in>
2024-08-21 08:22:00 +0530

    Fix Required validation on DTOs

--- postgres-init/03_Pension_Master_DML.sql
+++ postgres-init/03_Pension_Master_DML.sql
@@ -3,6 +3,7 @@ INSERT INTO cts_pension.primary_categories (
 	(1,'2071 - 01 - 101 - 00 - 005 - V - 04 - 00','College( Government) Pension',true),
   (4,'2071 - 01 - 109 - 00 - 001 - V - 04 - 00','Education Pension',true),
   (43,'2071 - 01 - 101 - 00 - 005 - V - 04 - 00','State Pension',true);
+SELECT setval('cts_pension.primary_categories_id_seq', 44, true);
 
 INSERT INTO cts_pension.sub_categories (
   id,sub_category_name,active_flag) VALUES
@@ -13,6 +14,7 @@ INSERT INTO cts_pension.sub_categories (
   (7,'Pension Rules 1966(Pre 81)',true),
   (8,'ROPA 2016',true),
   (12,'ROPA 2019',true);
+SELECT setval('cts_pension.sub_categories_id_seq', 13, true);
 
 INSERT INTO cts_pension.categories(
   id, primary_category_id, sub_category_id, category_name, active_flag)	VALUES 
@@ -22,6 +24,7 @@ INSERT INTO cts_pension.categories(
   (31, 4, 7, 'Education Pension-Pension Rules 1966(Pre 81)',true),
   (138, 4, 12, 'Education Pension-ROPA 2019',true),
   (48, 1, 2, 'College( Government) Pension-ROPA 2009',true);
+SELECT setval('cts_pension.categories_id_seq', 139, true);
 
 INSERT INTO cts_pension.breakups(
 	id, component_name, component_type, relief_flag, active_flag) VALUES 
@@ -33,6 +36,7 @@ INSERT INTO cts_pension.breakups(
   (6, 'ARREAR', 'P', false, true),
   (7, 'EXGRATIA', 'P', false, true),
   (8, 'AMOUNT COMMUTED', 'D', false, true);
+SELECT setval('cts_pension.breakups_id_seq', 9, true);
 
 INSERT INTO cts_pension.component_rates(
 	id, category_id, breakup_id, effective_from_date, rate_amount, rate_type, active_flag) VALUES 
@@ -46,18 +50,21 @@ INSERT INTO cts_pension.component_rates(
   (827, 30, 2, '2016-01-01', 156, 'P', true),
   (1204, 138, 2, '2019-12-01', 0, 'P', true),
   (1205, 138, 3, '2019-12-01', 500, 'A', true);
+SELECT setval('cts_pension.component_rates_id_seq', 1206, true);
 
 INSERT INTO cts_pension.ppo_receipt_sequences (
   id, financial_year, treasury_code, next_sequence_value, active_flag) VALUES
   (1, 2024, 'DAA', 2, true);
+SELECT setval('cts_pension.ppo_receipt_sequences_id_seq', 2, true);
 INSERT INTO cts_pension.ppo_receipts (
   financial_year,treasury_code,treasury_receipt_no,ppo_no,pensioner_name,date_of_commencement,mobile_number,receipt_date,psa_code,ppo_type,ppo_status,created_by,active_flag) VALUES
   (2024,'DAA','DAA2024000001','PPO-955487','Jack Dowsel','2024-07-15','9090445140','2024-08-01','A','N','PPO Received',39,true);
-SELECT nextval('cts_pension.ppo_receipts_id_seq');
+SELECT setval('cts_pension.ppo_receipts_id_seq', 2, true);
 INSERT INTO cts_pension.pensioners 
   (id, financial_year, treasury_code, receipt_id, ppo_id, ppo_no, ppo_type, ppo_sub_type, category_id, pensioner_name, date_of_birth, gender, mobile_number, email_id, pensioner_address, identification_mark, pan_no, aadhaar_no, date_of_retirement, date_of_commencement, basic_pension_amount, commuted_pension_amount, enhance_pension_amount, reduced_pension_amount, religion, created_by, active_flag) VALUES
   (1, 2024, 'DAA', 1, 1, 'PPO-296128', 'P', 'N', 30, 'Jack Dowsel', '2024-08-10', 'M', '8882371949', 'string', 'Malda', 'S', 'PANNO4614F', '945148468446', '1997-03-25', '1997-03-26', 10000, 2000, 10000, 8000, 'H', 39, true);
-SELECT nextval('cts_pension.pensioners_id_seq');
+SELECT setval('cts_pension.pensioners_id_seq', 2, true);
 INSERT INTO cts_pension.ppo_id_sequences 
 (id, financial_year, treasury_code, next_sequence_value, active_flag) VALUES
 (1, 0, 'DAA', 1, true);
+SELECT setval('cts_pension.ppo_id_sequences_id_seq', 2, true);
