e6f06f068d094c9428904a046a35cea4eb54b2bc
Abu Salam [Lenovo-Slim3i] <abu.alam@nic.in>
2024-09-16 21:05:08 +0530

    Write Tests for Manual PPO CRUD APIs

--- dotnet/CTS-BE.Tests/Controllers/PpoReceiptControllerTests.cs
+++ dotnet/CTS-BE.Tests/Controllers/PpoReceiptControllerTests.cs
@@ -15,7 +15,7 @@ namespace CTS_BE.Tests.Controllers
     public class PpoReceiptControllerTests : BaseControllerTests
     {
         [Fact]
-        public async Task PensionController_ControlManualPpoReceiptsCreate_CanCreate()
+        public async Task PpoReceiptController_CreatePpoReceipt_CanCreate()
         {
             // Arrange
             ManualPpoReceiptEntryDTO manualPpoReceiptEntryDTO = new PpoReceiptFactory().Create();
@@ -36,6 +36,93 @@ namespace CTS_BE.Tests.Controllers
             responseData.Should().BeOfType<JsonAPIResponse<ManualPpoReceiptResponseDTO>>();
             responseData?.ApiResponseStatus.Should().Be(Enum.APIResponseStatus.Success);
             responseData?.Message.Should().Be("PPO Received Successfully!");
+            responseData?.Result.Should().BeEquivalentTo(responseResult);
+        }
+       
+        [Fact]
+        public async Task PpoReceiptController_UpdatePpoReceipt_CanUpdate()
+        {
+            // Arrange
+            ManualPpoReceiptEntryDTO manualPpoReceiptEntryDTO = new PpoReceiptFactory().Create();
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? savedReceipt = 
+                await CallPostAsJsonAsync<ManualPpoReceiptResponseDTO, ManualPpoReceiptEntryDTO>(
+                    "/api/v1/manual-ppo/receipts",
+                    manualPpoReceiptEntryDTO
+                );
+
+            ManualPpoReceiptEntryDTO receiptUpdateDTO = new PpoReceiptFactory().Create();
+
+            // Act
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? responseData = 
+                await CallPutAsJsonAsync<ManualPpoReceiptResponseDTO, ManualPpoReceiptEntryDTO>(
+                    $"/api/v1/manual-ppo/receipt/{savedReceipt?.Result?.Id ?? 0}",
+                    receiptUpdateDTO
+                );
+
+            ManualPpoReceiptResponseDTO? responseResult = responseData?.Result;
+            responseResult.FillFrom(receiptUpdateDTO);
+
+            // Assert
+            using (new AssertionScope())
+            responseData.Should().NotBeNull();
+            responseData.Should().BeOfType<JsonAPIResponse<ManualPpoReceiptResponseDTO>>();
+            responseData?.ApiResponseStatus.Should().Be(Enum.APIResponseStatus.Success);
+            responseData?.Result.Should().BeEquivalentTo(responseResult);
+        }
+
+        [Fact]
+        public async Task PpoReceiptController_GetPpoReceiptById_CanGet()
+        {
+            // Arrange
+            ManualPpoReceiptEntryDTO manualPpoReceiptEntryDTO = new PpoReceiptFactory().Create();
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? savedReceipt = 
+                await CallPostAsJsonAsync<ManualPpoReceiptResponseDTO, ManualPpoReceiptEntryDTO>(
+                    "/api/v1/manual-ppo/receipts",
+                    manualPpoReceiptEntryDTO
+                );
+
+            // Act
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? responseData = 
+                await CallGetAsJsonAsync<ManualPpoReceiptResponseDTO>(
+                    $"/api/v1/manual-ppo/receipt/{savedReceipt?.Result?.Id ?? 0}"
+                );
+
+            ManualPpoReceiptResponseDTO? responseResult = responseData?.Result;
+            responseResult.FillFrom(manualPpoReceiptEntryDTO);
+
+            // Assert
+            using (new AssertionScope())
+            responseData.Should().NotBeNull();
+            responseData.Should().BeOfType<JsonAPIResponse<ManualPpoReceiptResponseDTO>>();
+            responseData?.ApiResponseStatus.Should().Be(Enum.APIResponseStatus.Success);
+            responseData?.Result.Should().BeEquivalentTo(responseResult);
+        }
+
+        [Fact]
+        public async Task PpoReceiptController_GetPpoReceiptByTreasuryReceiptNo_CanGet()
+        {
+            // Arrange
+            ManualPpoReceiptEntryDTO manualPpoReceiptEntryDTO = new PpoReceiptFactory().Create();
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? savedReceipt = 
+                await CallPostAsJsonAsync<ManualPpoReceiptResponseDTO, ManualPpoReceiptEntryDTO>(
+                    "/api/v1/manual-ppo/receipts",
+                    manualPpoReceiptEntryDTO
+                );
+
+            // Act
+            JsonAPIResponse<ManualPpoReceiptResponseDTO>? responseData = 
+                await CallGetAsJsonAsync<ManualPpoReceiptResponseDTO>(
+                    $"/api/v1/manual-ppo/receipts/{savedReceipt?.Result?.TreasuryReceiptNo ?? "null"}"
+                );
+
+            ManualPpoReceiptResponseDTO? responseResult = responseData?.Result;
+            responseResult.FillFrom(manualPpoReceiptEntryDTO);
+
+            // Assert
+            using (new AssertionScope())
+            responseData.Should().NotBeNull();
+            responseData.Should().BeOfType<JsonAPIResponse<ManualPpoReceiptResponseDTO>>();
+            responseData?.ApiResponseStatus.Should().Be(Enum.APIResponseStatus.Success);
             responseData?.Result.Should().BeEquivalentTo(responseResult);
         }
        
