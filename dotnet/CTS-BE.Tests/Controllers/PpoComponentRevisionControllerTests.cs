59ed19a45cbfedbe7e33fa682dd2bf7732481776
Abu Salam [Lenovo-Slim3i] <abu.alam@nic.in>
2024-10-09 16:06:50 +0530

    Create Get PPO List API for Component Revision

--- dotnet/CTS-BE.Tests/Controllers/PpoComponentRevisionControllerTests.cs
+++ dotnet/CTS-BE.Tests/Controllers/PpoComponentRevisionControllerTests.cs
@@ -9,6 +9,73 @@ namespace CTS_BE.Tests.Controllers
 {
     public class PpoComponentRevisionControllerTests : BaseControllerTests
     {
+
+        [Fact]
+        public async Task PpoComponentRevisionController_GetAllPposForComponentRevisions_CanGetAsync()
+        {
+            // Arrange
+            PensionerEntryDTO pensionerEntryDTO = new PensionerFactory().Create();
+            ManualPpoReceiptEntryDTO? ppoReceipt = new PpoReceiptFactory().Create();
+            pensionerEntryDTO.PpoNo = ppoReceipt.PpoNo;
+            pensionerEntryDTO.CategoryId = 30;
+            ppoReceipt.DateOfCommencement = pensionerEntryDTO.DateOfCommencement;
+            _ = await CallPostAsJsonAsync<ManualPpoReceiptResponseDTO, ManualPpoReceiptEntryDTO>(
+                    "/api/v1/manual-ppo/receipts",
+                    ppoReceipt
+                );
+            JsonAPIResponse<PensionerResponseDTO>? pensioner = await CallPostAsJsonAsync<PensionerResponseDTO, PensionerEntryDTO>(
+                "/api/v1/ppo/details",
+                pensionerEntryDTO
+            );
+            
+            int ppoId = pensioner?.Result?.PpoId ?? 0;
+
+            PensionStatusEntryDTO pensionStatusEntryDTO = new () {
+                PpoId = ppoId,
+                StatusFlag = PensionStatusFlag.PpoApproved,
+                StatusWef = DateOnly.FromDateTime(DateTime.Now)
+            };
+            _ = await CallPostAsJsonAsync<PensionStatusEntryDTO, PensionStatusEntryDTO>(
+                "/api/v1/ppo/status",
+                pensionStatusEntryDTO
+            );
+
+            InitiateFirstPensionBillDTO initiateFirstPensionBillDTO = new () {
+                PpoId = ppoId,
+                ToDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+
+            _ = await CallPostAsJsonAsync<InitiateFirstPensionBillResponseDTO, InitiateFirstPensionBillDTO>(
+                $"/api/v1/ppo/first-bill",
+                initiateFirstPensionBillDTO
+            );
+
+
+            PpoBillEntryDTO ppoBillEntryDTO = new () {
+                PpoId = ppoId,
+                ToDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+
+            _ = await CallPostAsJsonAsync<PpoBillSaveResponseDTO, PpoBillEntryDTO>(
+                $"/api/v1/ppo/pension-bill",
+                ppoBillEntryDTO
+            );
+
+
+            // Act
+            JsonAPIResponse<TableResponseDTO<PpoComponentRevisionPpoListItemDTO>>? responseData 
+                = await CallGetAsJsonAsync<TableResponseDTO<PpoComponentRevisionPpoListItemDTO>>(
+                    $"/api/v1/ppo/component-revision/ppos"
+                );
+
+            // Assert
+            using (new AssertionScope())
+            responseData?.Result.Should().NotBeNull();
+            responseData?.Result?.DataCount.Should().BeGreaterThan(0);
+            responseData?.ApiResponseStatus.Should().Be(Enum.APIResponseStatus.Success);
+        }
+
+
         [Fact]
         public async Task PpoComponentRevisionController_CreateSinglePpoComponentRevision_CanCreateAsync()
         {
